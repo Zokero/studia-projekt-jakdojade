68e0890deef0542cf5a82fe6823968c7962233f0
Zokero <piotrekkukiela@gmail.com>
2018-11-01 09:31:19 +0100

    Change project to springboot

---
+++ src/main/java/com/studia/projekt/studiaprojektjakdojade/StudiaProjektJakdojadeApplication.java
@@ -0,0 +1,12 @@
+package com.studia.projekt.studiaprojektjakdojade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiaProjektJakdojadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiaProjektJakdojadeApplication.class, args);
+	}
+}
